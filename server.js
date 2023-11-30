7e04972ea50e2f0f62492cbb3375c038c9f4cc1b
metie <lesw1012@gmail.com>
2023-11-30 10:57:45 +0900

    make it better

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');  // path 모듈 추가
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public'), { 'Content-Type': 'application/javascript' }));
+
+// 정적 파일 제공 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
+wss.on('connection', ws => {
+    console.log('클라이언트 연결');
+
+    // 클라이언트로부터 메시지를 수신할 때
+    ws.on('message', message => {
+        console.log(`수신한 메시지: ${message}`);
+
+        // 수신한 메시지를 다른 클라이언트에게 전달
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+server.listen(3000, '0.0.0.0', () => {
+    console.log('시그널링 서버 시작: http://localhost:3000');
+});
